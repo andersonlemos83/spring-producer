1b539ecce6a336ed5a8290da33535c99e04f17c9
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2021-02-08 15:51:31 -0300

    Implementa produto do RabbitMQ

---
+++ src/main/java/com/example/spring/producer/dto/Message.java
@@ -0,0 +1,21 @@
+package com.example.spring.producer.dto;
+
+public class Message {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "text='" + text + '\'' +
+                '}';
+    }
+}
